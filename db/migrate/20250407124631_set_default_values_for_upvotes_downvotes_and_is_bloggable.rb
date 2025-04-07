eb0d39f2f03be7277a21bc39ca2374b8ec91e28d
Chandan Kumar Mandal <chandan.mandal@pixelcompute.com>
2025-04-07 13:21:39 +0000

    feat: Add Post model with migrations and tests
    - Created `Post` model in `app/models/post.rb`.
    - Added migration `20250407104730_create_posts.rb` to create the posts table.
    - Added migration `20250407124348_make_title_description_upvotes_downvotes_not_nullable.rb` to set NOT NULL constraints on title, description, upvotes, and downvotes columns.
    - Added migration `20250407124631_set_default_values_for_upvotes_downvotes_and_is_bloggable.rb` to set default values for upvotes, downvotes, and is_bloggable columns.
    - Updated `db/schema.rb` to reflect the latest database schema changes.
    - Added `Post` model tests in `test/models/post_test.rb`.
    
    Signed-off-by: Chandan Kumar Mandal <chandan.mandal@pixelcompute.com>

---
+++ db/migrate/20250407124631_set_default_values_for_upvotes_downvotes_and_is_bloggable.rb
@@ -0,0 +1,7 @@
+class SetDefaultValuesForUpvotesDownvotesAndIsBloggable < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default :posts, :upvotes, from: nil, to: 0
+    change_column_default :posts, :downvotes, from: nil, to: 0
+    change_column_default :posts, :is_bloggable, from: nil, to: false
+  end
+end
