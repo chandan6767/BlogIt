60a971359387e365e39f8852476875d9ada0fbbb
Chandan Kumar Mandal <chandan.mandal@pixelcompute.com>
2025-04-09 14:04:16 +0000

    Added git hooks, eslint, prettier and rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 125
   MAX_DESCRIPTION_LENGTH = 10000
   VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9].*\z/i
   VALID_BLOGGABLE_VALUES = [true, false].freeze
 
-  validates :title, 
-    presence: true, 
+  validates :title,
+    presence: true,
     length: { maximum: MAX_TITLE_LENGTH },
     format: { with: VALID_TITLE_REGEX }
-  validates :description, 
+  validates :description,
     presence: true,
     length: { maximum: MAX_DESCRIPTION_LENGTH }
   validates_inclusion_of :is_bloggable,
-      in: VALID_BLOGGABLE_VALUES
+    in: VALID_BLOGGABLE_VALUES
 end
--- db/migrate/20250407124348_make_title_description_upvotes_downvotes_not_nullable.rb
+++ db/migrate/20250407124348_make_title_description_upvotes_downvotes_not_nullable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MakeTitleDescriptionUpvotesDownvotesNotNullable < ActiveRecord::Migration[7.1]
   def change
     change_column_null :posts, :title, false
