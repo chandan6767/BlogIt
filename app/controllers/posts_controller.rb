aad51195bb64dce5f2eb77d65ffca96fb64f7f70
Chandan Kumar Mandal <chandan.mandal@pixelcompute.com>
2025-04-17 04:05:59 +0000

    Replace two-step post creation with `Post.create!` for simplicity
    Signed-off-by: Chandan Kumar Mandal <chandan.mandal@pixelcompute.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    post.save!
+    Post.create!(post_params)
     render_notice(t("successfully_created"))
   end
 
